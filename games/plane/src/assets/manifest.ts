5c0721712fc1f8fb3200f92a2aa0862d68d94096
憨憨 <zhi75426@gmail.com>
2026-05-16 00:16:42 +0800

    M4a-3 plane 拷贝 hero 资产并定义 AssetManifest

---
+++ games/plane/src/assets/manifest.ts
@@ -0,0 +1,7 @@
+import type { AssetManifest } from '@cp/core';
+
+export const planeManifest: AssetManifest = {
+    images: [
+        { key: 'hero', url: 'static/飞机png/hero/plane_01_blue_striker_hires.png' }
+    ]
+};
